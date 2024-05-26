c3b496284ebaa5c62e37393433d29bb6bc3578b4
Somto Ufondu <somtoufondu@gmail.com>
2024-05-26 17:58:18 -0600

    Download the .sql files with the fake data in them

--- 0_selects/1_students_without_github.sql
+++ 0_selects/1_students_without_github.sql
@@ -1,11 +1,4 @@
-SELECT
-  id,
-  name,
-  email,
-  cohort_id
-FROM
-  students
-WHERE
-  github IS NULL
-ORDER BY
-  cohort_id
+SELECT id, name, email, cohort_id
+FROM students
+WHERE github IS NULL
+ORDER BY cohort_id
