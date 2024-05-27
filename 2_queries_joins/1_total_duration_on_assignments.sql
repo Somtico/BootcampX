cd22504c2f2569032fcab3adde58530c884024ae
Somto Ufondu <somtoufondu@gmail.com>
2024-05-27 06:35:10 -0600

    Start the JOIN statement on a new line

--- 2_queries_joins/1_total_duration_on_assignments.sql
+++ 2_queries_joins/1_total_duration_on_assignments.sql
@@ -1,3 +1,4 @@
 SELECT SUM(assignment_submissions.duration) AS total_duration
-FROM assignment_submissions JOIN students ON students.id = student_id
+FROM assignment_submissions
+JOIN students ON students.id = student_id
 WHERE students.name = 'Ibrahim Schimmel';
